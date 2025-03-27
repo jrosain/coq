d2c26695ce1ee01fe68005d8cdc1f5bfac84d6ab
Johann Rosain <johann.rosain@gmail.com>
2025-03-27 15:27:35 +0100

    Kernel: removal of families, factorisation of elimination

--- vernac/vernacentries.ml
+++ vernac/vernacentries.ml
@@ -1365,7 +1365,7 @@ let add_subnames_of ?loc len n ns full_n ref =
         | exception Not_found -> ns
         | None -> ns
         | Some ref -> (len, ref) :: ns)
-      ns Sorts.all_families
+      ns Sorts.Quality.all
 
 let interp_names m ns =
   let dp_m = Nametab.dirpath_of_module m in
